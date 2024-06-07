314352f93df2951a43c64f195dd452a361943c44
Ragavendran Ramalingam <ragaven.r@gmail.com>
2024-06-07 14:28:45 +0530

    server health check inclusion

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net"
-
 	"rail-booking/internal"
-	"rail-booking/protogen/booking"
+	bk "rail-booking/protogen/booking"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main() {
@@ -27,9 +29,29 @@ func main() {
 	st := internal.NewAllocateSeat(db)
 	railBookingService := internal.NewRailBookingService(db, st)
 
-	// register the rail booking service with the grpc server
-	booking.RegisterBookingServiceServer(server, &railBookingService)
+	healthServer := health.NewServer()
 
+	go func() {
+		for {
+			status := healthpb.HealthCheckResponse_SERVING
+			// Check if user Service is valid
+			if time.Now().Second()%2 == 0 {
+				status = healthpb.HealthCheckResponse_NOT_SERVING
+			}
+
+			healthServer.SetServingStatus(bk.BookingService_ServiceDesc.ServiceName, status)
+			healthServer.SetServingStatus("", status)
+
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	healthServer.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
+	healthServer.SetServingStatus(bk.BookingService_ServiceDesc.ServiceName, healthpb.HealthCheckResponse_SERVING)
+
+	// register the rail booking service with the grpc server
+	bk.RegisterBookingServiceServer(server, &railBookingService)
+	healthpb.RegisterHealthServer(server, healthServer)
 	// start listening to requests
 	log.Printf("server listening at %v", listener.Addr())
 	if err = server.Serve(listener); err != nil {
